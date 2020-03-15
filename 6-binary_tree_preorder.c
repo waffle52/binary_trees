433988e34b632c6d336da13a0e33df52543ad9eb
waffle <asuka9767@protonmail.com>
2020-03-15 02:49:35 +0000

    changed print function

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		printf("%d\n", tree->n);
+		print_num(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
 	}
