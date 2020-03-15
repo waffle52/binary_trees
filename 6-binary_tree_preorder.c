febc87431c3797eadd5675fa08c1f8498ff85f26
waffle <asuka9767@protonmail.com>
2020-03-15 02:33:30 +0000

    Uploading for testing

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - Entry Point
+ * @tree: pointer to root node
+ * @func: function to pointer to call with each node
+ * Description: uses pre-order traversal to go through tree)?
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		printf("%d\n", tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
